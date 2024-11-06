221a1a6394fd6e28bb97378be92392a16250eb45
SpaceLudens <kurmanrm@gmail.com>
2024-11-06 18:56:15 +0500

    updated UriToURL.java

--- app/src/main/java/hexlet/code/util/UriToURL.java
+++ app/src/main/java/hexlet/code/util/UriToURL.java
@@ -1,13 +1,21 @@
 package hexlet.code.util;
 
-import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URI;
 
 public class UriToURL {
-    public static String parserUriToUrl(String uri) throws MalformedURLException, URISyntaxException {
+    public static String parserUriToUrl(String uri) throws  URISyntaxException {
         URI parsedUri = new URI(uri);
-        parsedUri.toURL();
-        return parsedUri.getScheme() + "://" + parsedUri.getHost();
+
+        String protocol = parsedUri.getScheme();
+        String host = parsedUri.getHost();
+        int port = parsedUri.getPort();
+
+        StringBuilder baseUrl = new StringBuilder(protocol + "://" + host);
+
+        if (port != -1) {
+            baseUrl.append(":").append(port);
+        }
+        return baseUrl.toString();
     }
 }
